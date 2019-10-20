117f166099a7e77d7de57bf20b0fd1f3d99d7028
Christian Jimenez <christianjimenez0123@gmail.com>
2019-10-20 20:51:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,7 +25,7 @@ end
 def using_concat(my_favorite_things)
   all_my_favs = my_favorite_things.concat(more_favs)
 end
-def using_insert
+def using_insert(list_of_programming_languages)
 new_array = list_of_programming_languages.insert(another_language)
 end
 def using_uniq
