1a8f77499c0346f7110e190b2650ca2b2fe4cb7d
Christian Jimenez <christianjimenez0123@gmail.com>
2019-10-20 20:50:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,10 +23,10 @@ def shift_with_args(ice_cream_brands)
 brands_removed =  ice_cream_brands.shift(2)
 end
 def using_concat(my_favorite_things)
-  all_my_favs = my_favorite_things.concat(["sports cars", "flatiron school"])
+  all_my_favs = my_favorite_things.concat(more_favs)
 end
 def using_insert
-
+new_array = list_of_programming_languages.insert(another_language)
 end
 def using_uniq
 
