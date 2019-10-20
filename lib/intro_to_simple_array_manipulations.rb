99c828e2bb1595894e36d090bea3f22ed4e30570
Christian Jimenez <christianjimenez0123@gmail.com>
2019-10-20 20:40:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,6 @@ end
 def shift_with_args(ice_cream_brands)
 brands_removed =  ice_cream_brands.shift(2)
 end
-def using_concat(my_favorite_things)
+def using_concat(all_my_favs)
   all_my_favs = my_favorite_things.concat(more_favs)
 end
