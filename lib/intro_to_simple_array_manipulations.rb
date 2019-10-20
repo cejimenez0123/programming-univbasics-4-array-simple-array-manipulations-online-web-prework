b0f8a22dc32dd850b547507eebe78c596c42daa6
Christian Jimenez <christianjimenez0123@gmail.com>
2019-10-20 21:28:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -34,9 +34,9 @@ end
 def using_flatten(instruments)
   flat_array = instruments.flatten
 end
-def using_delete(instructors)
+def using_delete(instructors,no_offense_steven)
   no_offense_steven = instructors.delete("Steven")
 end
-def using_delete_at(famous_robots)
+def using_delete_at(famous_robots, deleted_robot)
   deleted_robot = famous_robots.delete_at(-1)
 end
