5b4a63115771fd5659d868af6beebe58bbbd720a
Christian Jimenez <christianjimenez0123@gmail.com>
2019-10-20 21:08:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,7 @@ def using_concat(my_favorite_things,more_favs)
   all_my_favs = my_favorite_things.concat(more_favs)
 end
 def using_insert(list_of_programming_languages,another_language)
-  another_language = "Phyton"
-new_array = list_of_programming_languages.insert(another_language)
+new_array = list_of_programming_languages.insert(-1,another_language)
 end
 def using_uniq(haircuts)
   new_array = haircuts.uniq
@@ -37,5 +36,7 @@ def using_flatten(instruments)
 end
 def using_delete(instructors)
   no_offense_steven = instructors.delete("Steven")
+end
+def using_delete_at
 
 end
